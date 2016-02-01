4d692e524ff0f0cb3d85348abdac32356f56ccae
Shufei <shufei@gotenzing.com>
2016-02-01 17:15:41 -0500

    register mobile page

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ $view->parserExtensions = array(
     new \Slim\Views\TwigExtension()
 );
 
+/////Create normal version pages//////////////////////////
 $surprising = new Page("surprising", "surprising", "pages/surprising.html");
 $whoswho = new Page("whoswho", "whoswho", "pages/whoswho.html");
 $branding = new Page("branding", "branding", "pages/branding.html");
@@ -34,33 +35,54 @@ $contact = new Page("contact", "contact", "pages/contact.html");
 
 $listItems = array();
 array_push($listItems, $surprising, $whoswho, $branding,$engaging, $orienteering, $integrating, $positioning,$humanchanneling, $serving, $spending);
-$pageList = new pageHandle($listItems);
+$pageList = new PageHandle($listItems);
 
 $utilityPages = [];
 array_push($utilityPages,$personalitiesBios, $galleryLegacy, $servicesSkills, $privacy, $contact);
 $utilityPageList = new PageHandle($utilityPages);
 
-$app->get('/', function() use ($app, $pageList,$utilityPageList) {
+///////create mobile version pages/////////
+$surprising_mobile = new Page("surprising-mobile", "surprising-mobile", "mobile-pages/surprising-mobile.html"); 
+$whoswho_mobile = new Page("whoswho-mobile", "whoswho-mobile", "mobile-pages/whoswho-mobile.html");
+$branding_mobile = new Page("branding-mobile", "branding-mobile", "mobile-pages/branding-mobile.html");
+$engaging_mobile = new Page("engaging-mobile", "engaging-mobile", "mobile-pages/engaging-mobile.html");
+$orienteering_mobile = new Page("orienteering-mobile", "orienteering-mobile", "mobile-pages/orienteering-mobile.html");
+$integrating_mobile = new Page("integrating-mobile", "integrating-mobile", "mobile-pages/integrating-mobile.html");
+$positioning_mobile = new Page("positioning-mobile", "positioning-mobile", "mobile-pages/positioning-mobile.html");
+$humanchanneling_mobile = new Page("humanchanneling-mobile", "humanchanneling-mobile", "mobile-pages/humanchanneling-mobile.html");
+$serving_mobile = new Page("serving-mobile", "serving-mobile", "mobile-pages/serving-mobile.html");
+$spending_mobile = new Page("spending-mobile", "spending-mobile", "mobile-pages/spending-mobile.html");
+
+
+$mobilePages = array();
+array_push($mobilePages, $surprising_mobile,$whoswho_mobile, $branding_mobile, $engaging_mobile, $orienteering_mobile, $integrating_mobile, $positioning_mobile, $humanchanneling_mobile, $serving_mobile, $spending_mobile);
+$mobilePageList = new PageHandle($mobilePages);
+
+
+$app->get('/', function() use ($app, $pageList,$utilityPageList,$mobilePageList) {
     $app->render('templates.html', [
         'pageList' => $pageList,
-        'utilityPageList' => $utilityPageList
+        'utilityPageList' => $utilityPageList,
+    	'mobilePageList'  => $mobilePageList	
     ]);
 })->setName('templates');
 
-$app->get('/:name', function ($name) use ($app, $pageList, $utilityPageList) {
+$app->get('/:name', function ($name) use ($app, $pageList, $utilityPageList, $mobilePageList) {
             $app->render('templates.html',[
                 'name' => $name,
                 'pageList' => $pageList,
-                'utilityPageList' => $utilityPageList
+                'utilityPageList' => $utilityPageList,
+            	'mobilePageList' => $mobilePageList	
             ]);
 
 })->setName('subtemplates');
 
-$app->get('/utility/:name', function ($name) use ($app, $pageList, $utilityPageList) {
+$app->get('/utility/:name', function ($name) use ($app, $pageList, $utilityPageList, $mobilePageList) {
     $app->render('templates.html',[
         'name' => $name,
         'pageList' => $pageList,
-        'utilityPageList' => $utilityPageList
+        'utilityPageList' => $utilityPageList,
+    	'mobilePageList' => $mobilePageList
     ]);
 
 })->setName('subtemplates');
