9aeb019dfb3467886ea1475c2172cb6d2e64784b
Insu Mun <insu@gotenzing.com>
2016-02-26 16:00:20 -0500

    Normalize carousel image height.

--- templates/src/js/main.js
+++ templates/src/js/main.js
@@ -1,18 +1,53 @@
 (function ($, viewport) {
     "use strict";
 
+    // Normalize Carousel Heights - pass in Bootstrap Carousel items.
+    $.fn.carouselHeights = function() {
+
+        var items = $(this), //grab all slides
+            heights = [], //create empty array to store height values
+            tallest; //create variable to make note of the tallest slide
+
+        var normalizeHeights = function() {
+
+            items.each(function() { //add heights to array
+                heights.push($(this).height());
+            });
+            tallest = Math.max.apply(null, heights); //cache largest value
+            items.each(function() {
+                $(this).css('min-height',tallest + 'px');
+            });
+        };
+
+        normalizeHeights();
+
+        $(window).on('resize orientationchange', function () {
+            //reset vars
+            tallest = 0;
+            heights.length = 0;
+
+            items.each(function() {
+                $(this).css('min-height','0'); //reset min-height
+            });
+            normalizeHeights(); //run it again
+        });
+
+    };
+
     // Reset cookie
     $.removeCookie('previousUrl', {'path': '/'});
     $.removeCookie("utilityMenuOpen");
 
     $(window).load(function() {
+        $('.carousel-inner .item').carouselHeights();
+
         new LazyLoad({
             skip_invisible: false
         });
-
     });
 
     $(document).ready(function () {
+
         window.isGalleryOpen = false;
 
         initFirstClickMenuAnimation();
@@ -102,7 +137,7 @@
             // If the page is utility page with the orange menu down
             $(".arrow-down, .extension-header").slideDown();
             $(".shadow-main").show();
-            $(".menuicon").parent("li").addClass('orange');
+            $(".menuicon").addClass('is-active').parent("li").addClass('orange');
 
             $('.div-exlist').find('#utility-' + link).addClass('selected');
             $('.div-iconlist').find('#utility-' + link).parent().addClass('orange');
