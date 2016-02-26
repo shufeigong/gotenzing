39672f04a3815ff7a21aebea3b8a3eb9ccb1d722
Insu Mun <insu@gotenzing.com>
2016-02-26 16:24:39 -0500

    Prevent to run the normalize image height twice.

--- templates/src/js/main.js
+++ templates/src/js/main.js
@@ -1,37 +1,30 @@
 (function ($, viewport) {
     "use strict";
 
-    // Normalize Carousel Heights - pass in Bootstrap Carousel items.
+    // Normalize Bootstrap Carousel Heights
     $.fn.carouselHeights = function() {
-
-        var items = $(this), //grab all slides
-            heights = [], //create empty array to store height values
-            tallest; //create variable to make note of the tallest slide
-
+        var items = $(this).find('.item'), // grab all slides
+            heights = [], // create empty array to store height values
+            tallest, // create variable to make note of the tallest slide
+            call;
         var normalizeHeights = function() {
-
-            items.each(function() { //add heights to array
-                heights.push($(this).height());
+            items.each(function() { // add heights to array
+                heights.push($(this).outerHeight());
             });
-            tallest = Math.max.apply(null, heights); //cache largest value
-            items.each(function() {
-                $(this).css('min-height',tallest + 'px');
-            });
+            tallest = Math.max.apply(null, heights); // cache largest value
+            items.css('height', tallest);
         };
-
         normalizeHeights();
-
-        $(window).on('resize orientationchange', function () {
-            //reset vars
+        $(window).on('resize orientationchange', function() {
+            // reset vars
             tallest = 0;
             heights.length = 0;
-
-            items.each(function() {
-                $(this).css('min-height','0'); //reset min-height
-            });
-            normalizeHeights(); //run it again
+            items.css('height', ''); // reset height
+            if(call){
+                clearTimeout(call);
+            };
+            call = setTimeout(normalizeHeights, 100); // run it again
         });
-
     };
 
     // Reset cookie
@@ -39,7 +32,9 @@
     $.removeCookie("utilityMenuOpen");
 
     $(window).load(function() {
-        $('.carousel-inner .item').carouselHeights();
+        $('.carousel').each(function(){
+            $(this).carouselHeights();
+        });
 
         new LazyLoad({
             skip_invisible: false
